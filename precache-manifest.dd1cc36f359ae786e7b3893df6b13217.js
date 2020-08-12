87d09a071c38b19c6eef33eca034511c0f3e0f23
unknown <shireesha.vani@gmail.com>
2020-08-12 21:53:49 +0530

    Updates

---
+++ precache-manifest.dd1cc36f359ae786e7b3893df6b13217.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feeb2bd1f2667e6a3ac131ef3e7f2922",
+    "url": "/Covid-react/index.html"
+  },
+  {
+    "revision": "f8a59fe293195fbe0e64",
+    "url": "/Covid-react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c7b8896cf79203f4a5c9",
+    "url": "/Covid-react/static/css/main.b091fcb8.chunk.css"
+  },
+  {
+    "revision": "f8a59fe293195fbe0e64",
+    "url": "/Covid-react/static/js/2.9affe3e3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Covid-react/static/js/2.9affe3e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7b8896cf79203f4a5c9",
+    "url": "/Covid-react/static/js/main.80dd1cff.chunk.js"
+  },
+  {
+    "revision": "25fff994676ec997e3ef",
+    "url": "/Covid-react/static/js/runtime-main.66f098b0.js"
+  }
+]);
